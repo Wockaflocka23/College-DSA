3ccb5e4ca7c874a23754944eddfa242546d202d2
Wockaflocka23 <140996587+Wockaflocka23@users.noreply.github.com>
2025-05-27 10:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (26.19%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,38 +1,15 @@
 class Solution {
-
-    // Transpose the matrix
-    private void transpose(int[][] matrix) {
-        int n = matrix.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                // Swap matrix[i][j] and matrix[j][i]
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-    }
-
-    // Reverse each row
-    private void reverseRows(int[][] matrix) {
-        int n = matrix.length;
-
-        for (int i = 0; i < n; i++) {
-            int left = 0, right = n - 1;
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-                left++;
-                right--;
-            }
-        }
-    }
-
-    // Main method to rotate matrix by 90 degrees clockwise
     public void rotate(int[][] matrix) {
-        transpose(matrix);
-        reverseRows(matrix);
+        int n=matrix.length;
+        int[][] sol=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+              sol[j][n-1-i]=matrix[i][j];
+            }
+        }
+         for (int i = 0; i < n; i++) {
+            System.arraycopy(sol[i], 0, matrix[i], 0, n);
+        }
+    
     }
-}
+    }
